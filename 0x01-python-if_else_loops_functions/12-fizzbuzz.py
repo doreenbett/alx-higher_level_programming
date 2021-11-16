fea2eae25428d0f77a7704e6e47e447c627b3255
doreenbett <doreenbett001@gmail.com>
2021-11-16 22:35:19 +0000

    Fixing spaces

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def fizzbuzz():
-   for number in range(1, 101):
-#For multiples of three and five, print Fizzbuzz instead of te number
-      if number % 3 == 0 and number % 5 == 0:
-          print("FizzBuzz ", end="")
-#for multiples of three , print Fizz instead of te number
-      elif number % 3 == 0:
-          print("Fizz ", end="")
-#For multiples of five, print Buzz instead of the number
-      elif number % 5 == 0:
-          print("Buzz ", end="")
-      else:
-          print("{} ".format(number), end="")
+    """For multiples of five, print Buzz instead of the number  
+    For multiples of three , print Fizz instead of te number
+    For multiples of three and five, print Fizzbuzz instead of the number
+    """
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
