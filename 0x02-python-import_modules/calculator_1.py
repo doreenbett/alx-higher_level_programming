4eb42009a2913af414c2d3da19ed71ac394febf6
doreenbett <doreenbett001@gmail.com>
2021-11-18 20:28:36 +0000

    Adding calc.py

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+	The return values of a + b
+    """
+    return (a, b):
+
+def sub(a, b):
+    """Subtraction function
+    Args:
+        a: first int
+	b: second int
+    Returns:
+	The return value of a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """Multiplication function
+
+    Args:
+        a: first int
+	b: second int
+
+    Returns:
+        The return value of a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """Division function
+    Args:
+	a: first int
+        b: second int
+    Returns:
+	The return value of a / b
+    """
+    return int(a / b)
