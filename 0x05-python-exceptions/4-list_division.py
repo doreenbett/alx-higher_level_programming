8f6f21f21d42ed9c84e32dfddfbd2d656d4b525c
doreenbett <doreenbett001@gmail.com>
2021-12-06 19:07:21 +0000

    Fixed codes

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,13 +11,13 @@ def list_division(my_list_1, my_list_2, list_length):
             print(result)
         except TypeError:
             print("wrong type")
-            div = 0
+            result = 0
         except ZeroDivisionError:
             print("division by 0")
-            div = 0
+            result = 0
         except IndexError:
             print("out of range")
-            div = 0
+            result = 0
         finally:
             new_list.append(result)
     return (new_list)
