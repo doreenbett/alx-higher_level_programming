bdae6960e7491fd6f81c753b52e66a79d5f76629
doreenbett <doreenbett001@gmail.com>
2021-12-22 10:40:29 +0000

    Modified code

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """Defines an inherited list class Mylist."""
 
 
-class Mylist(list):
+class MyList(list):
     """Implements sorted printing for the  built-in list class"""
 
     def print_sorted(self):
