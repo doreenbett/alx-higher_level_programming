cc383e5de2161192567a9fe7f342764fc8f76cdc
doreenbett <doreenbett001@gmail.com>
2022-01-04 19:25:31 +0000

    Modified code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,7 +94,7 @@ class Rectangle(Base):
             [print("#", end="") for w in range(self.width)]
             print("")
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update the Rectangle.
         Args:
             *args (ints): New attribute values
@@ -103,6 +103,7 @@ class Rectangle(Base):
                 :3rd argument rep height attribute
                 :4th argument rep x attribute
                 :5th argument rep y attribute
+            **kwargs (dict): New key/value pairs of attributes
         """
         if args and len(args) != 0:
             a = 0
@@ -121,6 +122,22 @@ class Rectangle(Base):
                 elif a == 4:
                     self.y = arg
                 a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
