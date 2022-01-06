a4b684d55adc2d816b2e3b54b181e9ec2989f986
doreenbett <doreenbett001@gmail.com>
2022-01-06 15:55:07 +0000

    Modified code

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts
+        Args: list_dictionaries (list): A list of dictionaries
+        """
+        if list_dictionaries is None orlist_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
