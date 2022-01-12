c11e991482869a9d03ac43da3f21745192edf4b0
doreenbett <doreenbett001@gmail.com>
2022-01-12 10:24:47 +0000

    Fixed code

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Intialize a new Rectangle.
         Args:
-        width (int): The width of the new Rectangle.
-        height (int): The height of the new Rectangle.
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
         """
         self.integer_validator("width", width)
         self.__width = width
