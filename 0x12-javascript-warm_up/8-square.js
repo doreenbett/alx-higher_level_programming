b473c554374868eaebf838c2464c46795ba2482b
doreenbett <doreenbett001@gmail.com>
2022-02-28 19:27:21 +0000

    Modified code

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
-	  console.log('Missing size');
+  console.log('Missing size');
 } else {
   for (let r = 0; r < size; r++) {
     let row = '';
