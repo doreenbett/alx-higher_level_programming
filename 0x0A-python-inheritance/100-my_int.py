568a69c192ccc2b10d229a352f6c6d3edd89d1de
doreenbett <doreenbett001@gmail.com>
2022-06-28 11:00:59 +0000

    Modified code

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-MyInt = __import__('100-my_int').MyInt
+# 100-my_int.py
+"""Defines a class MyInt that inherits from int."""
 
-my_i = MyInt(3)
-print(my_i)
-print(my_i == 3)
-print(my_i != 3)
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
